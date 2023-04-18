ca2ae584e4a22468b2256644d655cadc6c4a9b77
Patrick McPherson <patrick.j.mcpherson@gmail.com>
2023-04-18 11:10:23 -0700

    ProjectCard init

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,7 +1,55 @@
 import React from "react";
+import Image, { StaticImageData } from "next/image";
 
-function ProjectCard() {
-    return <div>ProjectCard</div>;
+function ProjectCard({
+    image,
+    name,
+    description,
+    frontend,
+    backend,
+    database,
+    liveLink,
+    repoLink,
+}: {
+    image: StaticImageData;
+    name: string;
+    description: string;
+    frontend: string;
+    backend: string;
+    database: string;
+    liveLink: string;
+    repoLink: string;
+}) {
+    return (
+        <div className="relative w-80 overflow-hidden rounded-lg border-2 border-eerieBlack bg-charcoal shadow-inner shadow-eerieBlack">
+            <div className="relative h-72">
+                <Image className="mb-1 place-self-center" src={image} alt={name} fill />
+            </div>
+            <div className="mx-2">
+                <strong className="text-lg font-bold">{name.toUpperCase()}</strong>
+                <p className="text-sm">{description}</p>
+                <ul>
+                    <li>Frontend: {frontend}</li>
+                    <li>Backend: {backend}</li>
+                    <li>Database: {database}</li>
+                </ul>
+                <div>
+                    <div>
+                        <a href={liveLink} target="_blank" rel="noopener noreferrer">
+                            <span>icon</span>
+                            <span>LIVE SITE</span>
+                        </a>
+                    </div>
+                    <div>
+                        <a href={repoLink} target="_blank" rel="noopener noreferrer">
+                            <span>icon</span>
+                            <span>REPOSITORY</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default ProjectCard;
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,10 +1,26 @@
 import React from "react";
+import ProjectCard from "./ProjectCard";
+import coffeePosts from "../public/images/projects/coffeePosts.jpeg";
 
 function Projects() {
+    const projects = [
+        {
+            name: "Project 1",
+            image: coffeePosts,
+            description: "This is a description of project 1",
+            frontend: "React",
+            backend: "Flask",
+            database: "PostgreSQL",
+            liveLink: "https://www.google.com",
+            repoLink: "https://www.google.com",
+        },
+    ];
     return (
         <div
             className={`mx-auto flex w-11/12 flex-wrap justify-center gap-2 overflow-hidden transition-all duration-500`}>
-            Projects
+            {projects.map((project, index) => (
+                <ProjectCard {...project} key={index} />
+            ))}
         </div>
     );
 }
